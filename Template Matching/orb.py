34e5b64ef868f182a06410d6ccb891406df64e1a
ssmarika <smarikashrestha02@gmail.com>
2024-11-27 12:55:35 +0545

    Working

--- Template Matching/orb.py
+++ Template Matching/orb.py
@@ -24,7 +24,7 @@ def resize_image(image, width=None, height=None):
 
 # Load the template and uploaded images
 template_image = cv2.imread(r'C:\Users\uSer\Desktop\SmartKYC\Template Matching\temp1.jpg', cv2.IMREAD_GRAYSCALE)
-uploaded_image = cv2.imread(r'C:\Users\uSer\Desktop\SmartKYC\Template Matching\fake2.jpg', cv2.IMREAD_GRAYSCALE)
+uploaded_image = cv2.imread(r'C:\Users\uSer\Desktop\SmartKYC\Template Matching\uploaded_document.jpg', cv2.IMREAD_GRAYSCALE)
 
 # Initialize ORB detector
 orb = cv2.ORB_create()
